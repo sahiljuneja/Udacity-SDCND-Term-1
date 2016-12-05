31ed582b995b4912b986082819d888a3f2338203
sjuneja <sahiljuneja17@gmail.com>
2016-12-05 01:20:21 -0800

    update move command

--- Term-1/P2-Traffic-Sign-Classifier/docker_ubuntu/get_data.sh
+++ Term-1/P2-Traffic-Sign-Classifier/docker_ubuntu/get_data.sh
@@ -4,5 +4,5 @@ curl -LOk https://d17h27t6h515a5.cloudfront.net/topher/2016/October/580d53ce_tra
 
 sudo apt-get install unzip
 unzip traffic-sign-data.zip
-sudo mv lab\ 2\ data/ /home/ubuntu/Udacity-Self-Driving-Car-ND/Term-1/P2-Traffic-Sign-Classifier/
+sudo mv lab\ 2\ data/* /home/ubuntu/Udacity-Self-Driving-Car-ND/Term-1/P2-Traffic-Sign-Classifier/
 
