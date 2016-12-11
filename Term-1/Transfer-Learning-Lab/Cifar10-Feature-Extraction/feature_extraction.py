facbb718b13322c57ac1f7d36d1b8cf6925557b6
Ubuntu <ubuntu@ip-172-31-39-13.ec2.internal>
2016-12-11 14:19:16 +0000

    add single layer model

--- Term-1/Transfer-Learning-Lab/Cifar10-Feature-Extraction/feature_extraction.py
+++ Term-1/Transfer-Learning-Lab/Cifar10-Feature-Extraction/feature_extraction.py
@@ -1,6 +1,15 @@
 import pickle
 import tensorflow as tf
+import numpy as np
+import cv2
 # TODO: import Keras layers you need here
+
+from keras.models import Sequential
+from keras.layers.core import Dense, Dropout, Activation, Flatten
+from keras.layers import Convolution2D, MaxPooling2D
+from keras.optimizers import SGD, Adam, RMSprop
+from keras.utils import np_utils
+from keras.datasets import cifar10
 
 flags = tf.app.flags
 FLAGS = flags.FLAGS
@@ -46,9 +55,36 @@ def main(_):
     # the dataset
     # 10 for cifar10
     # 43 for traffic
+    def normalize_img(image):
+    	return cv2.normalize(image, None, alpha=-0.5, beta=0.5, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_32F)
 
+    X_train = np.array([normalize_img(image) for image in X_train], dtype=np.float32)
+    X_val = np.array([normalize_img(image) for image in X_val], dtype=np.float32)
+
+    ip_shape = X_train.shape[1:]
+    num_classes = len(np.unique(y_train))
+    num_neurons = 512
     # TODO: train your model here
 
+    model = Sequential()
+    model.add(Flatten(input_shape = ip_shape))
+    model.add(Dense(num_neurons))
+    model.add(Activation('relu'))
+    model.add(Dense(num_classes))
+    model.add(Activation('softmax'))
+
+    model.summary()
+
+    nb_epoch = 50
+    batch_size = 256
+
+    model.compile(loss='sparse_categorical_crossentropy',
+              optimizer=Adam(),
+              metrics=['accuracy'])
+
+    history = model.fit(X_train, y_train,
+                    batch_size=batch_size, nb_epoch=nb_epoch,
+                    verbose=1, validation_data=(X_val, y_val))
 
 # parses flags and calls the `main` function above
 if __name__ == '__main__':
