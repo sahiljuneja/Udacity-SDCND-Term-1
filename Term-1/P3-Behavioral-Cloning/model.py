0a2b27de4499a0d23243653c7a76c0fd77164116
sjuneja <sahiljuneja17@gmail.com>
2017-02-18 10:39:58 +0530

    update

--- Term-1/P3-Behavioral-Cloning/model.py
+++ Term-1/P3-Behavioral-Cloning/model.py
@@ -1,3 +1,8 @@
+
+# coding: utf-8
+
+# In[1]:
+
 ### Import
 import pickle
 import cv2
@@ -11,30 +16,39 @@ import random
 import numpy as np
 import pandas as pd
 import matplotlib.image as mpimg
+import matplotlib.pyplot as plt
 
 from sklearn.model_selection import train_test_split
 from sklearn.utils import shuffle
 from sklearn import preprocessing
 
 from keras.models import Sequential
-from keras.layers.core import Dense, Dropout, Activation, Flatten
+from keras.layers.core import Dense, Dropout, Activation, Flatten, Lambda
 from keras.layers import Convolution2D, MaxPooling2D
-from keras.optimizers import SGD, Adam, RMSprop
+from keras.optimizers import Adam
+from keras.callbacks import ModelCheckpoint
 from keras.utils import np_utils
 
-
 ### Load data
-#TODO Improve the following if possible.
 
 # Read in CSV file
-csv_loc = "data/driving_log.csv"
+csv_loc = "data_mine/driving_log.csv"
 df = pd.read_csv(csv_loc)
-df = df.drop(df[df['steering'] == 0].sample(frac=0.60).index)
-df = df.drop(df[df['throttle'] < 0.25].sample(frac=0.50).index)
-print(len(df.index))
-# Add c,l and r images.
+
+# Drop 75% of Steering Angles with value = 0
+df = df.drop(df[df['steering'] == 0].sample(frac=0.75).index)
+
+# Add c,l and r images to features variable.
 features_col = pd.concat([df['center'], df['left'].map(str.strip), df['right'].map(str.strip)])
 features_col = np.array(features_col.values.tolist())
+
+# Format image path in CSV
+features_col_new = []
+for idx in range(len(features_col)):
+    tmp = list(features_col[idx])
+    tmp[139] = "/"
+    features_col_new.append("".join(tmp))
+features_col_new = np.array(features_col_new)
 
 # Add steering angles for c,l,r with added shift for l and r images
 l_shift = 0.25
@@ -45,77 +59,59 @@ labels_l = df['steering'] + l_shift
 labels_col = pd.concat([labels_c, labels_l, labels_r])
 labels_col = np.array(labels_col.values.tolist())
 
-print("Length of Features: {0}, Labels: {1}".format(len(features_col), len(labels_col)))
+print("Length of Features: {0}, Labels: {1}".format(len(features_col_new), len(labels_col)))
 
 # Split csv data
-features_col, labels_col = shuffle(features_col, labels_col)
+features_col, labels_col = shuffle(features_col_new, labels_col)
 X_train, X_val, y_train, y_val = train_test_split(features_col, labels_col, test_size=0.15, random_state=42232) 
 
 # Read in image list
-images = os.listdir("data/IMG/")
+images = os.listdir("data_mine/IMG/")
+
 
 ### Pre-Process
-img_cols = 120
-img_rows = 60
-
-def data_trans(image, label):
-    trans_factor = 50*np.random.uniform() - 25 # Parameters set based on original image dimensions
-    trans_matrix = np.float32([[1,0,trans_factor],[0,1,trans_factor]])
-    image_trans = cv2.warpAffine(image,trans_matrix,(image.shape[1],image.shape[0]))
-
-    label = label + trans_factor/120
-    #print(label)
-    
-    return image_trans, label
-    
+img_cols = 32
+img_rows = 16 
     
 def preprocess_data(image, label, flag):
     # Crop Image
     image = image[60:140,:]
+      
+    # Change Colorspace to HSV
+    image = (cv2.cvtColor(image, cv2.COLOR_RGB2HSV))[:,:,1]
     
-    # Resize
+    # Resize Image
     image = cv2.resize(image, (img_cols, img_rows))
     
-    # Translate image and steering angle
-    if flag == "TRAIN":
-        image, label = data_trans(image, label)
-    
-    # Normalize
-    image = cv2.normalize(image, None, alpha=-0.5, beta=0.5, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_32F)
-    
     return image, label
+
 
 ### Helper Functions
 
 def image_generator(csv_features, csv_labels, flag):
     csv_features, csv_labels = shuffle(csv_features, csv_labels)
-    p = 1000
+    
     for idx in range(len(csv_features)):
         
         label = csv_labels[idx]
         
-        image = mpimg.imread("data/" + csv_features[idx])
+        image = mpimg.imread("data_mine/" + csv_features[idx][136:])
         image, label = preprocess_data(image, label, flag)
-        ''' 
-        if  p < 1020 and flag == "TRAIN":
-            mpimg.imsave("image" + str(p),image)
-            print(label)
-            p += 1
-        '''
+        
+        # Reshape
+        image = image.reshape((img_rows, img_cols) + (1,))
         yield image, label
 
-epoch_labels = []
 def train_data_generator(csv_features, csv_labels, batch_size):
     num_rows = int(len(csv_features))
     ctr = None
-    batch_x = np.zeros((batch_size, img_rows, img_cols, 3))
+    batch_x = np.zeros((batch_size, img_rows, img_cols, 1))
     batch_y = np.zeros(batch_size)
     while True:
         
         for i in range(batch_size):
             
             if ctr is None or ctr >= num_rows:
-                print("length of batch: {0}".format(len(batch_x)))
                 ctr = 0
                 images = image_generator(csv_features, csv_labels, "TRAIN")
             batch_x[i], batch_y[i] = next(images)
@@ -126,14 +122,13 @@ def train_data_generator(csv_features, csv_labels, batch_size):
 def valid_data_generator(csv_features, csv_labels, batch_size):
     num_rows = int(len(csv_features))
     ctr = None
-    batch_x = np.zeros((batch_size, img_rows, img_cols, 3))
+    batch_x = np.zeros((batch_size, img_rows, img_cols, 1))
     batch_y = np.zeros(batch_size)
     while True:
         
         for i in range(batch_size):
             
             if ctr is None or ctr >= num_rows:
-                print("length of batch: {0}".format(len(batch_x)))
                 ctr = 0
                 images = image_generator(csv_features, csv_labels, "VALID")
             batch_x[i], batch_y[i] = next(images)
@@ -141,43 +136,37 @@ def valid_data_generator(csv_features, csv_labels, batch_size):
         
         yield (batch_x, batch_y)
         
-print(X_train.shape)
-	
+
 ### Parameters
-layer_1_depth = 32
-layer_2_depth = 64
-layer_3_depth = 128
+layer_1_depth = 16
+layer_2_depth = 32
+
 filter_size_1 = 5
 filter_size_2 = 3
-num_neurons_1 = 512
-num_neurons_2 = 128
-epochs = 3
+
+num_neurons_1 = 32
+
+epochs = 20
 batch_size = 64
+
 samples_per_epoch = X_train.shape[0]
- 
+
+
 ### Model
 model = Sequential()
-model.add(Convolution2D(layer_1_depth, filter_size_2, filter_size_2, border_mode = 'valid', subsample = (2,2), input_shape = (img_rows, img_cols, 3)))
+model.add(Lambda(lambda x: x/255.-0.5,input_shape = (img_rows, img_cols, 1)))
+
+model.add(Convolution2D(layer_1_depth, filter_size_1, filter_size_1, border_mode = 'valid', subsample = (1,1)))
 model.add(Activation('elu'))
 model.add(MaxPooling2D(pool_size=(2,2)))
-model.add(Dropout(0.5))
+
 model.add(Convolution2D(layer_2_depth, filter_size_2, filter_size_2, border_mode = 'valid', subsample = (1,1)))
 model.add(Activation('elu'))
 model.add(MaxPooling2D(pool_size=(2,2)))
-model.add(Dropout(0.5))
-model.add(Convolution2D(layer_3_depth, filter_size_2, filter_size_2, border_mode = 'valid', subsample = (1,1)))
-model.add(Activation('elu'))
-model.add(MaxPooling2D(pool_size=(2,2)))
-model.add(Dropout(0.5))
 
 model.add(Flatten())
+
 model.add(Dense(num_neurons_1))
-model.add(Activation('elu'))
-model.add(Dropout(0.5))
-model.add(Dense(num_neurons_2))
-model.add(Activation('elu'))
-model.add(Dropout(0.5))
-model.add(Dense(32))
 model.add(Activation('elu'))
 model.add(Dropout(0.5))
 model.add(Dense(1))
@@ -188,17 +177,18 @@ model.compile(loss='mse',
               optimizer=Adam(lr = 0.0001),
               metrics=['mean_absolute_error'])
 
-### Save Model
-with open('model.json', 'w') as f:
-	json.dump(model.to_json(), f)                                           
 
-history = model.fit_generator(train_data_generator(X_train, y_train, batch_size), 
-											samples_per_epoch=samples_per_epoch, 
-											nb_epoch = epochs,
-											verbose = 1,
-											validation_data = valid_data_generator(X_val, y_val, batch_size),
-                                                                                        nb_val_samples=X_val.shape[0])
+### Train model
+checkpoint = ModelCheckpoint("model.h5", monitor='val_loss', verbose=0, 
+                                            save_best_only=True, save_weights_only=False, mode='auto')
+
+history = model.fit_generator(train_data_generator(X_train, y_train, batch_size),
+                              samples_per_epoch=samples_per_epoch,
+                              nb_epoch = epochs,
+                              verbose = 1, callbacks = [checkpoint],
+                              validation_data = valid_data_generator(X_val, y_val, batch_size),
+                              nb_val_samples=X_val.shape[0], pickle_safe=True)
 
 
-### Save weights
-model.save_weights('model.h5')
+
+
